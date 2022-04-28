b1cfa1b15039d1cbb7a484f9c08f4abb84f78f1a
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-04-28 14:45:27 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DrB1ackBeard'   --rewritten by oosayeroo
+description 'qb-burgershot for QBCore Created By DrB1ackBeard & Rewritten as qb-weedshop by oosayeroo'
+version '3.1.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'client/garage.lua',
+	'client/menus.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/*.lua'
+}
