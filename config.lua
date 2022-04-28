36953b1482ae440aff99f35c5d68bb484782e985
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-04-28 14:58:26 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,76 @@
+Config = {}
+
+Config.GaragePedLocations = {
+    {coords = vector4(372.18, -827.04, 29.29, 90.12)},
+}
+
+Config.Items = {
+label = "Shop",
+    slots = 8,
+    items = {
+        [1] = {
+            name = "lighter",
+            price = 10,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 1,
+        },
+        [2] = {
+            name = "empty_weed_bag",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 2,
+        },
+        [3] = {
+            name = "rolling-paper",
+            price = 2,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 3,
+        },
+        [4] = {
+            name = "weed_nutrition",
+            price = 40,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 4,
+        },
+        [5] = {
+            name = "gift-box",
+            price = 45,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 5,
+        },
+        [6] = {
+            name = "brownie-mix",
+            price = 8,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 6,
+        },
+        [7] = {
+            name = "aluminumcan",
+            price = 1,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 7,
+        },
+        [8] = {
+            name = "drink-mix",
+            price = 15,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 8,
+        },
+    }
+}
