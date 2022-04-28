2c8f4acd9676aefb025f373ea3c163e19f31b47f
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-04-28 14:54:18 +0100

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,89 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+RegisterServerEvent("qb-weedshop:bill:player")
+AddEventHandler("qb-weedshop:bill:player", function(playerId, amount)
+        local biller = QBCore.Functions.GetPlayer(source)
+        local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
+        local amount = tonumber(amount)
+        if biller.PlayerData.job.name == 'weedshop' then
+            if billed ~= nil then
+                if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
+                    if amount and amount > 0 then
+                        MySQL.Async.insert('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (:citizenid, :amount, :society, :sender)', {
+                            ['citizenid'] = billed.PlayerData.citizenid,
+                            ['amount'] = amount,
+                            ['society'] = biller.PlayerData.job.name,
+                            ['sender'] = biller.PlayerData.charinfo.firstname
+                        })
+                        TriggerClientEvent('qb-phone:RefreshPhone', billed.PlayerData.source)
+                        TriggerClientEvent('QBCore:Notify', source, 'Invoice Successfully Sent', 'success')
+                        TriggerClientEvent('QBCore:Notify', billed.PlayerData.source, 'New Invoice Received')
+                    else
+                        TriggerClientEvent('QBCore:Notify', source, 'Must Be A Valid Amount Above 0', 'error')
+                    end
+                else
+                    TriggerClientEvent('QBCore:Notify', source, 'You Cannot Bill Yourself', 'error')
+                end
+            else
+                TriggerClientEvent('QBCore:Notify', source, 'Player Not Online', 'error')
+            end
+        else
+            TriggerClientEvent('QBCore:Notify', source, 'No Access', 'error')
+        end
+end)
+
+QBCore.Functions.CreateCallback('qb-weedshop:server:get:ingredientjoint', function(source, cb)
+    local src = source
+    local Ply = QBCore.Functions.GetPlayer(src)
+    local lettuce = Ply.Functions.GetItemByName("streetweed")
+    local meat = Ply.Functions.GetItemByName("rolling-paper")
+    if lettuce ~= nil and meat ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+
+QBCore.Functions.CreateCallback('qb-weedshop:server:get:ingredientweed-drink', function(source, cb)
+    local src = source
+    local Ply = QBCore.Functions.GetPlayer(src)
+    local bun = Ply.Functions.GetItemByName("aluminumcan")
+    local tomato = Ply.Functions.GetItemByName("drink-mix")
+    local lettuce = Ply.Functions.GetItemByName("streetweed")
+    if bun ~= nil and lettuce ~= nil and tomato ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+
+QBCore.Functions.CreateCallback('qb-weedshop:server:get:ingredientgiftset', function(source, cb)
+    local src = source
+    local Ply = QBCore.Functions.GetPlayer(src)
+    local fries = Ply.Functions.GetItemByName("joint")
+    local heartstopper = Ply.Functions.GetItemByName("gift-box")
+    local software = Ply.Functions.GetItemByName("streetweed")
+    if fries ~= nil and heartstopper ~= nil and software ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+
+QBCore.Functions.CreateCallback('qb-weedshop:server:get:ingredientweedbrownie', function(source, cb)
+    local src = source
+    local Ply = QBCore.Functions.GetPlayer(src)
+    local fries = Ply.Functions.GetItemByName("brownie-mix")
+    local heartstopper = Ply.Functions.GetItemByName("streetweed")
+    if fries ~= nil and heartstopper ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+
+QBCore.Functions.CreateUseableItem("weed-gift-set", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("qb-weedshop:giftset", source, item.name)
+end)
