23d0e71b2d54020cc8c891a47896f102b70a3ffd
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-05-30 23:34:48 +0100

    new delivery update

--- config.lua
+++ config.lua
@@ -4,9 +4,23 @@ Config.WeedGaragePedLocations = {
     {coords = vector4(372.18, -827.04, 29.29, 90.12)},
 }
 
+Config.DeliveryWait = 10000
+Config.DeliveryPayment = math.random(500, 700)
+
+Config.DropOffPoint1 = vector3(-297.74, 379.84, 112.1)
+Config.DropOffPoint2 = vector3(119.37, 564.19, 183.96)
+Config.DropOffPoint3 = vector3(808.72, -163.69, 75.88)
+Config.DropOffPoint4 = vector3(1060.41, -378.3, 68.23)
+Config.DropOffPoint5 = vector3(850.27, -532.63, 57.93)
+Config.DropOffPoint6 = vector3(281.96, -1694.96, 29.65)
+Config.DropOffPoint7 = vector3(39.02, -1911.58, 21.95)
+Config.DropOffPoint8 = vector3(256.43, -2023.4, 19.27)
+Config.DropOffPoint9 = vector3(512.51, -1790.77, 28.92)
+Config.DropOffPoint10 = vector3(1193.52, -1622.37, 45.22)
+
 Config.Items = {
 label = "weedshop",
-    slots = 9,
+    slots = 8,
     items = {
         [1] = {
             name = "lighter",
@@ -71,14 +85,6 @@ label = "weedshop",
             info = {},
             type = "item",
             slot = 8,
-        },
-        [9] = {
-            name = "weed-grinder",
-            price = 20,
-            amount = 50,
-            info = {},
-            type = "item",
-            slot = 9,
         },
     }
 }
